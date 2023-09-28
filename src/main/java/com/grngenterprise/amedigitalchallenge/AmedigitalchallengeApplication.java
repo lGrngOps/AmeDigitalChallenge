dc99c4725399b0462b333e290747317198b1b900
Railson Magalhaes <railson.sistemas@gmail.com>
2023-09-28 16:02:04 -0300

    Projeto Iniciado

---
+++ src/main/java/com/grngenterprise/amedigitalchallenge/AmedigitalchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.grngenterprise.amedigitalchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmedigitalchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmedigitalchallengeApplication.class, args);
+	}
+
+}
